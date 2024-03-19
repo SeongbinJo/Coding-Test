f4e8e670e0746996f8ecfd4897572cae70fb4d2e
Seongbin <86182850+SeongbinJo@users.noreply.github.com>
2024-03-19 17:52:52 +0900

    [level 1] Title: 바탕화면 정리, Time: 1.66 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.swift
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.swift
@@ -4,8 +4,6 @@ func solution(_ wallpaper:[String]) -> [Int] {
     
     var answer: [Int] = []
     
-    let wallpaper: [String] = wallpaper
-    
     var wallpaperArray: [[Int]] = []
 
     var existFileCol: [Int] = [] //행에 파일이 존재하는 인덱스를 저장
@@ -15,19 +13,13 @@ func solution(_ wallpaper:[String]) -> [Int] {
         wallpaperArray.append(column.map{ Int(String([$0 == "." ? "0" : "1"]))! })
     }
     
-    //1이 나온 위치가 가장 작은 곳과 가장 큰 곳(열)을 체크, Column들 중 1을 가지고있는 최소 행과 최대 행을 체크. 이를 조합하면 됨.
     for (index, column) in wallpaperArray.enumerated() {
         if column.contains(1) {
-            //해당 행에 파일이 존재함.
             existFileCol.append(index)
             existFileRow.append(column.firstIndex(of: 1)!)
             existFileRow.append(column.lastIndex(of: 1)!)
         }
     }
-    print(existFileCol.min()!) //시작점의 행
-    print(existFileCol.max()! + 1)//끝점의 행
-    print(existFileRow.min()!)//시작점의 열
-    print(existFileRow.max()! + 1)//끝점의 열
     
     answer.append(existFileCol.min()!)
     answer.append(existFileRow.min()!)
