2f426c0b2d115e3a8c8d76ee8f42083ef6d43cd5
Seongbin <86182850+SeongbinJo@users.noreply.github.com>
2024-03-19 17:56:43 +0900

    Update 바탕화면 정리.swift
    - 이후에 다시 봐도 이해하기 용이하도록 주석 추가.
    ** "." 과 "#"을 0과 1로 바꾸지 않았어도 풀 수 있는 문제인데 print 했을 경우 보기 쉽게 한다고 그랬다. 다음엔 한번 더 생각하자.

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.swift
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.swift
@@ -13,18 +13,20 @@ func solution(_ wallpaper:[String]) -> [Int] {
         wallpaperArray.append(column.map{ Int(String([$0 == "." ? "0" : "1"]))! })
     }
     
+    //1이 나온 위치가 가장 작은 곳과 가장 큰 곳(열)을 체크, Column들 중 1을 가지고있는 최소 행과 최대 행을 체크. 이를 조합하면 됨.
     for (index, column) in wallpaperArray.enumerated() {
         if column.contains(1) {
+            //해당 행에 파일이 존재함.
             existFileCol.append(index)
             existFileRow.append(column.firstIndex(of: 1)!)
             existFileRow.append(column.lastIndex(of: 1)!)
         }
     }
     
-    answer.append(existFileCol.min()!)
-    answer.append(existFileRow.min()!)
-    answer.append(existFileCol.max()! + 1)
-    answer.append(existFileRow.max()! + 1)
+    answer.append(existFileCol.min()!) //시작점의 행
+    answer.append(existFileRow.min()!) //시작점의 열
+    answer.append(existFileCol.max()! + 1) //끝점의 행
+    answer.append(existFileRow.max()! + 1) //끝점의 열
     
     return answer
 }
